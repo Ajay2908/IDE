f87ff8060030faec26e0ebd0c7196e2ad4440336
Ajay2908 <ajayreddyramapuram@gmail.com>
2021-06-02 21:39:55 +0530

    improving

--- ide.js
+++ ide.js
@@ -3,6 +3,6 @@ let editor;
 
 window.onload = function () {
     editor = ace.edit("editor");
-    editor.setTheme('ace/theme/monokai');
+    editor.setTheme('ace/theme/solarized_light');
     editor.session.setMode('ace/mode/c_cpp')
 }
