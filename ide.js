def0683ca38b6664a46f2da8095ccc6631e65b37
Ajay2908 <ajayreddyramapuram@gmail.com>
2021-06-03 00:59:33 +0530

    improving

--- ide.js
+++ ide.js
@@ -3,7 +3,7 @@ let editor;
 
 window.onload = function () {
     editor = ace.edit("editor");
-    editor.setTheme('ace/theme/solarized_dark');
+    editor.setTheme('ace/theme/monokai');
     editor.session.setMode('ace/mode/c_cpp')
 }
 $('a[href$="#Modal"]').on("click", function () {
