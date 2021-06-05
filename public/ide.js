eb4a967a356b7a601af699512990c8468a33e32c
Ajay2908 <ajayreddyramapuram@gmail.com>
2021-06-05 15:32:01 +0530

    improving

--- public/ide.js
+++ public/ide.js
@@ -40,6 +40,7 @@ $(document).ready(function () {
     // themeSelector("monokai");
     // $('#langDropDown').text("cpp");
     // $('#themeDropDown').text("monokai");
+    
 
 });
 
@@ -157,6 +158,7 @@ function themeSelector(theme) {
 }
 
 function saveCode() {
+
     $('.editor').each(function (index) {
         editor = ace.edit(this);
         const code = editor.getSession().getValue();
@@ -197,12 +199,32 @@ function saveCode() {
             })
 
         }
-
-
-
     });
 
+
 }
+function runCode() {
+    var element = document.getElementById("Runbuttonspin");
+    element.innerHTML = `<div class="spinner-border text-warning text-left" role="status">
+    <span class="sr-only"></span>
+  </div>`
+
+    setTimeout(function () {
+        element.innerHTML =
+            `<div class="btn-nav mx-2" id="Runbuttonspin" ><a class="btn navbar-btn text-light" onclick="runCode();" style="background-color: #2a9d8f;"
+            href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                class="bi bi-braces " viewBox="1 1 16 16">
+                <path
+                    d="M2.114 8.063V7.9c1.005-.102 1.497-.615 1.497-1.6V4.503c0-1.094.39-1.538 1.354-1.538h.273V2h-.376C3.25 2 2.49 2.759 2.49 4.352v1.524c0 1.094-.376 1.456-1.49 1.456v1.299c1.114 0 1.49.362 1.49 1.456v1.524c0 1.593.759 2.352 2.372 2.352h.376v-.964h-.273c-.964 0-1.354-.444-1.354-1.538V9.663c0-.984-.492-1.497-1.497-1.6zM13.886 7.9v.163c-1.005.103-1.497.616-1.497 1.6v1.798c0 1.094-.39 1.538-1.354 1.538h-.273v.964h.376c1.613 0 2.372-.759 2.372-2.352v-1.524c0-1.094.376-1.456 1.49-1.456V7.332c-1.114 0-1.49-.362-1.49-1.456V4.352C13.51 2.759 12.75 2 11.138 2h-.376v.964h.273c.964 0 1.354.444 1.354 1.538V6.3c0 .984.492 1.497 1.497 1.6z" />
+            </svg>
+            Run</a></div>
+`
+
+    }, 1000)
+
+}
+
 
 
 
